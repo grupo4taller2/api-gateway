d0f7d2a94362cc0e948979fbbc1cdb4d879fcd47
mateoicalvo <macalvo@fi.uba.ar>
2022-10-13 06:34:59 -0300

    Update users GET logic

--- src/routes/users.js
+++ src/routes/users.js
@@ -92,7 +92,7 @@ const userGETSchema = {
       },
     },
     404: {
-        description: 'Success Response',
+        description: 'User not found',
         type: 'object',
         properties: {
             msg: {type: 'string', default: 'Error. User not found.'},
@@ -102,47 +102,64 @@ const userGETSchema = {
   },
 };
 
+async function getUserResponse() {
+  
+};
+
 async function usersGET(req, reply) {
   // FIXME: partially implemented
-  let responseData = {};
+  const responseData = {};
   let userResponse;
   try {
     userResponse = await axios.get(`${settings.SERVICE_USERS_URL}/users/${req.params.userID}`);
   } catch (error) {
     if (error.response && error.response.status === 404) {
-        return reply.status(404).send(
-                { msg : 'Error. User not found',
-                  userID : req.params.userID });
-      }
+      return reply.status(404).send(
+        {
+          msg: 'Error. User not found',
+          userID: req.params.userID,
+        },
+      );
+    }
   }
-  // FIXME: si 404 -> Return 404.
-  const username = userResponse.data.username;
-  responseData.username = username;
+  // FIXME: WTF por qué hay que escribir la siguiente linea para que ande
+  const { username } = userResponse.data;
+  responseData.username = userResponse.data.username;
   responseData.email = userResponse.data.email;
   responseData.first_name = userResponse.data.first_name;
   responseData.last_name = userResponse.data.last_name;
 
-  const riderResponse = await axios.get(`${settings.SERVICE_USERS_URL}/riders/${username}`);
-  responseData.rider_information = {};
-  responseData.rider_information.phone_number = riderResponse.data.phone_number;
-  responseData.rider_information.wallet = riderResponse.data.wallet;
-  responseData.rider_information.preferred_location_name = riderResponse.data.preferred_location_name;
+  const riderResponse = await axios.get(
+    `${settings.SERVICE_USERS_URL}/riders/${username}`,
+    { validateStatus: false },
+  );
 
-  const driverResponse = await axios.get(`${settings.SERVICE_USERS_URL}/drivers/${username}`,
-    { validateStatus: false });
+  if (riderResponse.status === 200) {
+    responseData.rider_information = {};
+    const riderInformation = responseData.rider_information;
+    riderInformation.phone_number = riderResponse.data.phone_number;
+    riderInformation.wallet = riderResponse.data.wallet;
+    riderInformation.preferred_location_name = riderResponse.data.preferred_location_name;
+  }
+
+  const driverResponse = await axios.get(
+    `${settings.SERVICE_USERS_URL}/drivers/${username}`,
+    { validateStatus: false },
+  );
 
   if (driverResponse.status === 200) {
     responseData.driver_information = {};
-    responseData.driver_information.phone_number = driverResponse.data.phone_number;
-    responseData.driver_information.wallet = riderResponse.data.wallet;
-    responseData.driver_information.preferred_location_name = riderResponse.data.preferred_location_name;
+    const driverInformation = responseData.driver_information;
+    driverInformation.phone_number = driverResponse.data.phone_number;
+    driverInformation.wallet = riderResponse.data.wallet;
+    driverInformation.preferred_location_name = riderResponse.data.preferred_location_name;
 
-    responseData.driver_information.car = {};
-    responseData.driver_information.car.plate = riderResponse.data.plate;
-    responseData.driver_information.car.manufacturer = riderResponse.data.manufacturer;
-    responseData.driver_information.carmodel = riderResponse.data.model;
-    responseData.driver_information.caryear_of_production = riderResponse.data.year_of_production;
-    responseData.driver_information.carcolor = riderResponse.data.color;
+    driverInformation.car = {};
+    driverInformation.car.plate = riderResponse.data.plate;
+    driverInformation.car.manufacturer = riderResponse.data.manufacturer;
+    driverInformation.carmodel = riderResponse.data.model;
+    driverInformation.caryear_of_production = riderResponse.data.year_of_production;
+    driverInformation.carcolor = riderResponse.data.color;
   }
 
   return reply.status(200)
