941cb23491fc33e1e40142caab68ba7e8955a15b
Mateo <macalvo@fi.uba.ar>
2022-10-22 22:47:16 -0300

    Return full username info for amdins

--- src/schemas/admin-schemas.js
+++ src/schemas/admin-schemas.js
@@ -1,4 +1,16 @@
 const adminSchema = {
+  username: {
+    type: 'string',
+    example: 'aUsername',
+  },
+  first_name: {
+    type: 'string',
+    example: 'Mateo',
+  },
+  last_name: {
+    type: 'string',
+    example: 'Calvo',
+  },
   email: {
     type: 'string',
     example: 'admin@provider.com',
