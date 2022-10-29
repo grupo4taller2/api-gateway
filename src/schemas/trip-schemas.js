5a865c45318f767ecc4b70877ab97b3b6d32f322
mateoicalvo <macalvo@fi.uba.ar>
2022-10-28 22:49:03 -0300

    Update shcemas

--- src/schemas/trip-schemas.js
+++ src/schemas/trip-schemas.js
@@ -1,7 +1,7 @@
 const riderRequestTripSchema = {
   rider_username: { type: 'string' },
-  rider_location_name: { type: 'string' },
-  rider_desired_destination_name: { type: 'string' },
+  rider_origin_address: { type: 'string' },
+  rider_destination_address: { type: 'string' },
   trip_type: { type: 'string' },
 };
 
@@ -14,10 +14,10 @@ const requestedByRiderTripSchema = {
     type: 'string',
     example: 'mateo95',
   },
-  location: {
+  origin: {
     type: 'object',
     properties: {
-      name: { type: 'string', example: 'Av. Paseo Colón 850, Buenos Aires' },
+      address: { type: 'string', example: 'Av. Paseo Colón 850, Buenos Aires' },
       latitude: { type: 'number', example: -34.6174635 },
       longitude: { type: 'number', example: -58.369979 },
     },
@@ -25,7 +25,7 @@ const requestedByRiderTripSchema = {
   destination: {
     type: 'object',
     properties: {
-      name: { type: 'string', example: 'Gral. Las Heras 2214, Buenos Aires' },
+      address: { type: 'string', example: 'Gral. Las Heras 2214, Buenos Aires' },
       latitude: { type: 'number', example: -34.5885454 },
       longitude: { type: 'number', example: -58.3984251 },
     },
