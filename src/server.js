6089a90ca8a7e6dc6968edd8664ed434b3e8e7b0
Mateo <macalvo@fi.uba.ar>
2022-11-01 07:24:37 -0300

    Linting

--- src/server.js
+++ src/server.js
@@ -3,25 +3,41 @@ const API_PREFIX = '/api/v1';
 
 const fastify = require('fastify');
 
+// Plugins
+const swagger = require('./plugins/swagger');
+const auth = require('./auth/firebase_auth_test');
+const testAuth = require('./auth/firebase_auth_test');
+
+// Routes
+const usersRoute = require('./routes/users/users');
+const adminsRoute = require('./routes/admins/admins');
+const ridersRoute = require('./routes/riders');
+const driversRoute = require('./routes/drivers');
+const locationsRoute = require('./routes/locations/locations');
+const tripsRoute = require('./routes/trips/trips');
+const healthcheckRoute = require('./routes/healthcheck');
+const resetRoute = require('./routes/reset');
+
 function buildServer() {
   const app = fastify({
     logger: true,
   });
 
-  app.register(require('./plugins/swagger'));
-  app.register(require('./auth/firebase_auth_test.js'));
-  app.register(require('./routes/users/users'), { prefix: API_PREFIX });
-  app.register(require('./routes/admins/admins'), { prefix: API_PREFIX });
-  app.register(require('./routes/riders'), { prefix: API_PREFIX });
-  app.register(require('./routes/drivers'), { prefix: API_PREFIX });
-  app.register(require('./routes/locations/locations'), { prefix: API_PREFIX });
-  app.register(require('./routes/trips/trips'), { prefix: API_PREFIX });
-  app.register(require('./routes/healthcheck'), { prefix: API_PREFIX });
+  app.register(swagger);
+  app.register(auth);
+  app.register(usersRoute, { prefix: API_PREFIX });
+  app.register(adminsRoute, { prefix: API_PREFIX });
+  app.register(ridersRoute, { prefix: API_PREFIX });
+  app.register(driversRoute, { prefix: API_PREFIX });
+  app.register(locationsRoute, { prefix: API_PREFIX });
+  app.register(tripsRoute, { prefix: API_PREFIX });
+  app.register(healthcheckRoute, { prefix: API_PREFIX });
 
+  // eslint-disable-next-line consistent-return
   app.addHook('preHandler', (req, res, done) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST");
-    res.header("Access-Control-Allow-Headers",  "*");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Headers', '*');
 
     const isPreflight = /options/i.test(req.method);
     if (isPreflight) {
@@ -37,15 +53,15 @@ function buildTestServer() {
   const app = fastify({
     logger: false,
   });
-  app.register(require('./auth/firebase_auth_test.js'));
-  app.register(require('./routes/users/users'), { prefix: API_PREFIX });
-  app.register(require('./routes/admins/admins'), { prefix: API_PREFIX });
-  app.register(require('./routes/riders'), { prefix: API_PREFIX });
-  app.register(require('./routes/drivers'), { prefix: API_PREFIX });
-  app.register(require('./routes/healthcheck'), { prefix: API_PREFIX });
-  app.register(require('./routes/locations/locations'), { prefix: API_PREFIX });
-  app.register(require('./routes/trips/trips'), { prefix: API_PREFIX });
-  app.register(require('./routes/reset'));
+  app.register(testAuth);
+  app.register(resetRoute);
+  app.register(usersRoute, { prefix: API_PREFIX });
+  app.register(adminsRoute, { prefix: API_PREFIX });
+  app.register(ridersRoute, { prefix: API_PREFIX });
+  app.register(driversRoute, { prefix: API_PREFIX });
+  app.register(locationsRoute, { prefix: API_PREFIX });
+  app.register(tripsRoute, { prefix: API_PREFIX });
+  app.register(healthcheckRoute, { prefix: API_PREFIX });
   return app;
 }
 
