a88fad4932e8a86f22f07f99c11ff7e9e74d3156
mateoicalvo <macalvo@fi.uba.ar>
2022-12-02 01:01:50 -0300

    remove log

--- src/routes/trips/trip-get.js
+++ src/routes/trips/trip-get.js
@@ -49,7 +49,6 @@ async function tripGet(req, reply) {
 
   const origin = locationFromResponse(tripResponse.data.origin);
   const destination = locationFromResponse(tripResponse.data.destination);
-  console.log(tripResponse)
 
   const responseBody = {};
   responseBody.trip_id = tripResponse.data.id;
